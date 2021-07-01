ec883095d2d841245c8b4ebd9f59426b2d68ed92
Riyaaz <rahmatullaha845@gmail.com>
2021-07-01 21:15:47 +0530

    Play-1.1.0

--- app/src/main/java/com/example/myplayer/Activity/MainActivity.java
+++ app/src/main/java/com/example/myplayer/Activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myplayer.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,28 +9,27 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import com.example.myplayer.R;
 import com.example.myplayer.databinding.ActivityMainBinding;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
-import com.karumi.dexter.listener.single.PermissionListener;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     private String[] items;
+    SharedPreferences sharedPref;
+    ArrayList<File> songs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,15 +50,69 @@ public class MainActivity extends AppCompatActivity {
             }
         }).check();
     }
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        SharedPreferences sharedPreferences=getSharedPreferences("icon",MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putBoolean("iconclick",false);
+        editor.apply();
+        sharedPref=getSharedPreferences("playstate",MODE_PRIVATE);
+        boolean val=sharedPref.getBoolean("isplay",false);
+        if (val){
+            binding.cardView2.setVisibility(View.VISIBLE);
+            binding.playimage.setOnClickListener(v -> {
+                SharedPreferences sharedPreferences1=getSharedPreferences("icon",MODE_PRIVATE);
+                SharedPreferences.Editor editor1=sharedPreferences1.edit();
+                editor1.putBoolean("iconclick",true);
+                editor1.apply();
+                startActivity(new Intent(MainActivity.this,PlaySongs.class).putExtra("songs",songs)
+                        .putExtra("pos",0));
+            });
+        }
+        else {
+            binding.cardView2.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SharedPreferences sharedPreferences1=getSharedPreferences("icon",MODE_PRIVATE);
+        SharedPreferences.Editor editor1=sharedPreferences1.edit();
+        editor1.putBoolean("iconclick",false);
+        editor1.apply();
+        sharedPref=getSharedPreferences("playstate",MODE_PRIVATE);
+        boolean val=sharedPref.getBoolean("isplay",false);
+        if (val){
+            binding.cardView2.setVisibility(View.VISIBLE);
+            binding.playimage.setOnClickListener(v -> {
+                SharedPreferences sharedPreferences=getSharedPreferences("icon",MODE_PRIVATE);
+                SharedPreferences.Editor editor=sharedPreferences.edit();
+                editor.putBoolean("iconclick",true);
+                editor.apply();
+                startActivity(new Intent(MainActivity.this,PlaySongs.class).putExtra("songs",songs)
+                        .putExtra("pos",0));
+            });
+        }
+        else {
+            binding.cardView2.setVisibility(View.GONE);
+        }
+    }
+
     public ArrayList<File> findSong(File file){
         ArrayList<File> arrayList=new ArrayList<>();
         File[] files=file.listFiles();
+        assert files != null;
         for (File file1:files){
             if (file1.isDirectory() && ! file1.isHidden()){
                 arrayList.addAll(findSong(file1));
             }
             else {
-                if (file1.getName().endsWith(".mp3") || file1.getName().endsWith(".wav")){
+                if (file1.getName().endsWith(".mp3") ||
+                        file1.getName().endsWith(".wav")
+                        ||file1.getName().endsWith(".m4a")||file1.getName().endsWith(".aac")
+                ||file1.getName().endsWith(".mp4")||file1.getName().endsWith(".flac")||file1.getName().endsWith(".wma")){
                     arrayList.add(file1);
                 }
             }
@@ -68,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
     public void ShowSongs(){
         ProgressDialog progressDialog=new ProgressDialog(this);
         progressDialog.show();
-        final ArrayList<File> songs=findSong(Environment.getExternalStorageDirectory());
+        songs=findSong(Environment.getExternalStorageDirectory());
         items=new String[songs.size()];
         for (int i=0;i<songs.size();i++){
             items[i]= songs.get(i).getName()
@@ -100,11 +154,24 @@ public class MainActivity extends AppCompatActivity {
         }
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            View view=getLayoutInflater().inflate(R.layout.songs_item,null);
+            @SuppressLint({"ViewHolder", "InflateParams"}) View view=getLayoutInflater().inflate(R.layout.songs_item,null);
             TextView textView=view.findViewById(R.id.song_name);
             textView.setSelected(true);
             textView.setText(items[position]);
             return view;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences sharedPreferences=getSharedPreferences("playstate",MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putBoolean("isplay",false);
+        editor.apply();
+        SharedPreferences sharedPreferences1=getSharedPreferences("icon",MODE_PRIVATE);
+        SharedPreferences.Editor editor1=sharedPreferences1.edit();
+        editor1.putBoolean("iconclick",false);
+        editor1.apply();
     }
 }
--- app/src/main/java/com/example/myplayer/Activity/PlaySongs.java
+++ app/src/main/java/com/example/myplayer/Activity/PlaySongs.java
@@ -2,7 +2,10 @@ package com.example.myplayer.Activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -18,10 +21,14 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.myplayer.R;
 import com.example.myplayer.databinding.ActivityPlaySongsBinding;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Objects;
+
 import static java.lang.Thread.sleep;
 public class PlaySongs extends AppCompatActivity{
     ActivityPlaySongsBinding binding;
@@ -37,13 +44,33 @@ public class PlaySongs extends AppCompatActivity{
     private String[] items;
     View view;
     ListView listView1;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    boolean val;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityPlaySongsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        binding.repeat.setOnClickListener(v -> {
+            sharedPreferences=getSharedPreferences("state",MODE_PRIVATE);
+            editor=sharedPreferences.edit();
+            if (repeat_flag==0){
+                binding.repeat.setImageResource(R.drawable.repeat_all);
+                repeat_flag=1;
+                editor.putInt("repeat",1);
+                Toast.makeText(this, "Repeat All", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                binding.repeat.setImageResource(R.drawable.repeat_one);
+                repeat_flag=0;
+                editor.putInt("repeat",0);
+                Toast.makeText(this, "Repeat One", Toast.LENGTH_SHORT).show();
+            }
+            editor.apply();
+        });
         if (mediaPlayer != null){
             mediaPlayer.start();
             mediaPlayer.release();
@@ -56,30 +83,40 @@ public class PlaySongs extends AppCompatActivity{
             e.printStackTrace();
         }
         pos=bundle.getInt("pos",0);
-        binding.songplay.setSelected(true);
-        Uri uri=Uri.parse(songs.get(pos).toString());
-        songName=songs.get(pos).getName();
-        binding.songplay.setText(songName);
-        mediaPlayer=MediaPlayer.create(getApplicationContext(),uri);
-        animation = AnimationUtils.loadAnimation(this, R.anim.animation1);
-        mediaPlayer.start();
-        binding.playimage.startAnimation(animation);
-        binding.btnPlay.setOnClickListener(this::onClick);
-        binding.seekbar.setMax(mediaPlayer.getDuration());
-        Thread thread=new Thread(() -> {
-            int totalduration=mediaPlayer.getDuration();
-            int currentpos=0;
-            while (currentpos<totalduration) {
-                try {
-                    currentpos = mediaPlayer.getCurrentPosition();
-                    binding.seekbar.setProgress(currentpos);
-                    sleep(500);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        if (!val) {
+            binding.songplay.setSelected(true);
+            Uri uri = Uri.parse(songs.get(pos).toString());
+            songName = songs.get(pos).getName();
+            binding.songplay.setText(songName);
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+            animation = AnimationUtils.loadAnimation(this, R.anim.animation1);
+            try {
+                mediaPlayer.start();
+                SharedPreferences sharedPreferences = getSharedPreferences("playstate", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("isplay", true);
+                editor.apply();
+            } catch (Exception e) {
+                Toast.makeText(this, "Error:-" + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
-        });
-        thread.start();
+            binding.playimage.startAnimation(animation);
+            binding.btnPlay.setOnClickListener(this::onClick);
+            binding.seekbar.setMax(mediaPlayer.getDuration());
+            Thread thread = new Thread(() -> {
+                int totalduration = mediaPlayer.getDuration();
+                int currentpos = 0;
+                while (currentpos < totalduration) {
+                    try {
+                        currentpos = mediaPlayer.getCurrentPosition();
+                        binding.seekbar.setProgress(currentpos);
+                        sleep(500);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            thread.start();
+        }
         binding.seekbar.getProgressDrawable().setColorFilter(getResources().getColor(android.R.color.holo_red_dark), PorterDuff.Mode.MULTIPLY);
         binding.seekbar.getThumb().setColorFilter(getResources().getColor(R.color.purple_500),
                 PorterDuff.Mode.SRC_IN);
@@ -94,7 +131,13 @@ public class PlaySongs extends AppCompatActivity{
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.pause();
                 mediaPlayer.seekTo(binding.seekbar.getProgress());
+                try {
+                    mediaPlayer.start();
+                }catch (Exception e){
+                    Toast.makeText(PlaySongs.this, "Error:-"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
         endtime=createTime(mediaPlayer.getDuration());
@@ -110,14 +153,19 @@ public class PlaySongs extends AppCompatActivity{
             }
         },delay);
         binding.songlist.setOnClickListener(this::onClick2);//songList
-        mediaPlayer.setOnCompletionListener(mp -> {
-           ChangeSong();
-        });
+        mediaPlayer.setOnCompletionListener(mp ->ChangeSong());
         sessionid=mediaPlayer.getAudioSessionId();
         UpdateUI();
-        binding.btnNext.setOnClickListener(view -> {
-            PlayNextSong();
-        });
+        binding.btnNext.setOnClickListener(view ->PlayNextSong());
+            mediaPlayer.setOnCompletionListener(mp -> {
+                sharedPreferences=getSharedPreferences("state",MODE_PRIVATE);
+                int state=sharedPreferences.getInt("repeat",1);
+                if (state==0) {
+                    Repeat_one();
+                } else {
+                    ChangeSong();
+                }
+            });
         binding.btnPrevious.setOnClickListener(view -> {
             mediaPlayer.stop();
             mediaPlayer.release();
@@ -126,13 +174,16 @@ public class PlaySongs extends AppCompatActivity{
             mediaPlayer=MediaPlayer.create(this,uri2);
             songName=songs.get(pos).getName();
             binding.songplay.setText(songName);
-            mediaPlayer.start();
+            try {
+                mediaPlayer.start();
+            }catch (Exception e){
+                Toast.makeText(this, "Error:-"+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
             binding.songend.setText(endtime);
             sessionid=mediaPlayer.getAudioSessionId();
             UpdateUI();
             binding.playimage.clearAnimation();
             binding.playimage.startAnimation(animation);
-
             endtime=createTime(mediaPlayer.getDuration());
             binding.songend.setText(endtime);
             final Handler handler1=new Handler();
@@ -145,8 +196,15 @@ public class PlaySongs extends AppCompatActivity{
                     handler1.postDelayed(this,delay1);
                 }
             },delay1);
-
-
+            mediaPlayer.setOnCompletionListener(mp -> {
+                sharedPreferences=getSharedPreferences("state",MODE_PRIVATE);
+                int state=sharedPreferences.getInt("repeat",1);
+                if (state==0) {
+                    Repeat_one();
+                } else {
+                    ChangeSong();
+                }
+            });
             binding.seekbar.setMax(mediaPlayer.getDuration());
             Thread thread1=new Thread(() -> {
                 int totalduration=mediaPlayer.getDuration();
@@ -183,11 +241,71 @@ public class PlaySongs extends AppCompatActivity{
             startActivity(Intent.createChooser(intent1,"Share Via"));
         });
     }
+    public void Repeat_one(){
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        Uri uri2=Uri.parse(songs.get(pos).toString());
+        mediaPlayer=MediaPlayer.create(this,uri2);
+        songName=songs.get(pos).getName();
+        binding.songplay.setText(songName);
+        try {
+            mediaPlayer.start();
+        }catch (Exception e){
+            Toast.makeText(this, "Error:-"+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        binding.songend.setText(endtime);
+        sessionid=mediaPlayer.getAudioSessionId();
+        UpdateUI();
+        binding.playimage.clearAnimation();
+        binding.playimage.startAnimation(animation);
+        endtime=createTime(mediaPlayer.getDuration());
+        binding.songend.setText(endtime);
+        final Handler handler1=new Handler();
+        final int delay1=1000;
+        handler1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String currentTime=createTime(mediaPlayer.getCurrentPosition());
+                binding.songstart.setText(currentTime);
+                handler1.postDelayed(this,delay1);
+            }
+        },delay1);
+        binding.seekbar.setMax(mediaPlayer.getDuration());
+        Thread thread1=new Thread(() -> {
+            int totalduration=mediaPlayer.getDuration();
+            int currentpos=0;
+            while (currentpos<totalduration) {
+                try {
+                    currentpos = mediaPlayer.getCurrentPosition();
+                    binding.seekbar.setProgress(currentpos);
+                    sleep(500);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread1.start();
+    }
     private void UpdateUI(){
         if (sessionid !=-1){
             binding.wave.setAudioSessionId(sessionid);
         }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sharedPreferences=getSharedPreferences("state",MODE_PRIVATE);
+        int state=sharedPreferences.getInt("repeat",1);
+        if (state==0) {
+            binding.repeat.setImageResource(R.drawable.repeat_one);
+        } else {
+            binding.repeat.setImageResource(R.drawable.repeat_all);
+        }
+        SharedPreferences sharedPreferences1=getSharedPreferences("icon",MODE_PRIVATE);
+        val=sharedPreferences1.getBoolean("iconclick",false);
+    }
+
     private void PlayNextSong() {
         mediaPlayer.stop();
         mediaPlayer.release();
@@ -197,13 +315,26 @@ public class PlaySongs extends AppCompatActivity{
         songName=songs.get(pos).getName();
         endtime=createTime(mediaPlayer.getDuration());
         binding.songplay.setText(songName);
-        mediaPlayer.start();
+        try {
+            mediaPlayer.start();
+        }catch (Exception e){
+            Toast.makeText(this, "Error:-"+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
         binding.songend.setText(endtime);
         sessionid=mediaPlayer.getAudioSessionId();
         UpdateUI();
         binding.playimage.clearAnimation();
         binding.playimage.startAnimation(animation);
         binding.seekbar.setMax(mediaPlayer.getDuration());
+        mediaPlayer.setOnCompletionListener(mp -> {
+            sharedPreferences=getSharedPreferences("state",MODE_PRIVATE);
+            int state=sharedPreferences.getInt("repeat",1);
+            if (state==0) {
+                Repeat_one();
+            } else {
+                ChangeSong();
+            }
+        });
         Thread thread=new Thread(() -> {
             int totalduration=mediaPlayer.getDuration();
             int currentpos=0;
@@ -220,17 +351,26 @@ public class PlaySongs extends AppCompatActivity{
         thread.start();
     }
     private void onClick(View view) {
+        SharedPreferences sharedPreferences=getSharedPreferences("playstate",MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
         if (mediaPlayer.isPlaying()){
             binding.btnPlay.setBackgroundResource(R.drawable.ic_play);
             mediaPlayer.pause();
             binding.playimage.clearAnimation();
+            editor.putBoolean("isplay",false);
         }
         else {
             binding.btnPlay.setBackgroundResource(R.drawable.ic_pause);
-            mediaPlayer.start();
+            try {
+                mediaPlayer.start();
+            }catch (Exception e){
+                Toast.makeText(this, "Error:-"+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
             songName=songs.get(pos).getName();
             binding.playimage.startAnimation(animation);
+            editor.putBoolean("isplay",true);
         }
+        editor.apply();
     }
     public String createTime(int dur){
         String time="";
@@ -253,12 +393,11 @@ public class PlaySongs extends AppCompatActivity{
 
     @Override
     protected void onDestroy() {
-        if(binding.wave !=null){
-            binding.wave.release();
-        }
+        binding.wave.release();
         super.onDestroy();
     }
 
+    @SuppressLint("InflateParams")
     private void onClick2(View v) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         view = LayoutInflater.from(this).inflate(R.layout.song_list_holder, null);
@@ -282,7 +421,11 @@ public class PlaySongs extends AppCompatActivity{
             Uri uri1 = Uri.parse(songs.get(pos).toString());
             mediaPlayer = MediaPlayer.create(this, uri1);
             endtime = createTime(mediaPlayer.getDuration());
-            mediaPlayer.start();
+            try {
+                mediaPlayer.start();
+            }catch (Exception e){
+                Toast.makeText(this, "Error:-"+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
             binding.songend.setText(endtime);
             sessionid = mediaPlayer.getAudioSessionId();
             UpdateUI();
@@ -304,7 +447,13 @@ public class PlaySongs extends AppCompatActivity{
             });
             thread.start();
             mediaPlayer.setOnCompletionListener(mp -> {
-                ChangeSong();
+                sharedPreferences=getSharedPreferences("state",MODE_PRIVATE);
+                int state=sharedPreferences.getInt("repeat",1);
+                if (state==0) {
+                    Repeat_one();
+                } else {
+                    ChangeSong();
+                }
             });
             dialog.dismiss();
         });
@@ -319,7 +468,11 @@ public class PlaySongs extends AppCompatActivity{
         songName=songs.get(pos).getName();
         endtime=createTime(mediaPlayer.getDuration());
         binding.songplay.setText(songName);
-        mediaPlayer.start();
+        try {
+            mediaPlayer.start();
+        }catch (Exception e){
+            Toast.makeText(this, "Error:-"+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
         binding.songend.setText(endtime);
         sessionid=mediaPlayer.getAudioSessionId();
         UpdateUI();
@@ -342,7 +495,7 @@ public class PlaySongs extends AppCompatActivity{
         }
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            View view=getLayoutInflater().inflate(R.layout.songs_item,null);
+            @SuppressLint({"ViewHolder", "InflateParams"}) View view=getLayoutInflater().inflate(R.layout.songs_item,null);
             TextView textView=view.findViewById(R.id.song_name);
             textView.setSelected(true);
             textView.setText(items[position]);
